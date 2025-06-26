0d36163b0c00d32ba87b11d56676335b107692e8
andrei-popov03 <andrei.ivanov.popov@gmail.com>
2025-06-26 15:26:53 +0300

    Making some progress

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,27 @@
-#rom flask import Flask
-from conf import Conf
-#from  extantions import 
+from flask import Flask
+from config import Config
+from extensions import db, jwt, migrate
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register blueprints or routes here
+    from routes import auth_bp
+    app.register_blueprint(auth_bp)
+    
+    with app.app_context():
+        from models import User  # Import models to register them with SQLAlchemy
+        db.create_all()  # Create database tables
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000 ,debug=True)
--- backend/conf.py
+++ backend/conf.py
@@ -1,3 +1,9 @@
-import os 
+import os
+from .env_template import load_dotenv
 
-#Puhi e golem igra4
+load_dotenv()
+
+class Config:
+    SECRET_KEY=os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
